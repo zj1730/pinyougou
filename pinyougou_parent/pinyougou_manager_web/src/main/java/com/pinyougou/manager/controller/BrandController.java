2a67965050188fd8656112cbf6dddf1593b928e4
admin <2251049291@qq.com>
2019-02-14 09:54:42 +0800

    day01-基本工程创建

---
+++ pinyougou_parent/pinyougou_manager_web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -0,0 +1,22 @@
+package com.pinyougou.manager.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.BrandService;
+import com.pinyougou.pojo.TbBrand;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/brand")
+public class BrandController {
+
+    @Reference
+    private BrandService brandService;
+    @RequestMapping("/findAll")
+    public List<TbBrand> findAll(){
+
+        return brandService.findAll();
+    }
+}
---
+++ pinyougou_parent/pinyougou_sellergoods_interface/src/main/java/com/pinyougou/BrandService.java
@@ -0,0 +1,10 @@
+package com.pinyougou;
+
+import com.pinyougou.pojo.TbBrand;
+
+import java.util.List;
+
+public interface BrandService {
+
+    List<TbBrand> findAll();
+}
